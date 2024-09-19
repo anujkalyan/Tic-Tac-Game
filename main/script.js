cba63f06f26ef0694cdd5210bc76fea0569ba554
Anuj Kumar <92213114+anujkalyan@users.noreply.github.com>
2024-09-19 21:02:31 +0530

    script.js

--- main/script.js
+++ main/script.js
@@ -5,57 +5,67 @@ let gameover = new Audio("gameover.mp3")
 let turn = "X"
 let isgameover = false;
 
-const changeTurn = ()=>{
-
-    return turn === "X"? "0": "X"
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
 }
 
-const checkwin = ()=>{
-        let boxtext = document.getElementsByClassName('boxtext');
-        let wins = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],  
-            [2,4,6],
-        ]
-        wins.forEach(e =>{
-            if((boxtext[e[0]].innerText === boxtext[e[1]].innerText)&&(boxtext[e[2]].innerText === boxtext[e[1]].innerText)&&(boxtext[e[0]].innerText !== "")){
-               document.querySelector('.info').innerText = boxtext[e[0]].innerText + " won"
-                isgameover = true
-                document.querySelector('.imgbox').getElementsByTagName("img")[0].style.width = "200px"
-            }
-
-        })
+const checkwin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) &&
+            (boxtext[e[2]].innerText === boxtext[e[1]].innerText) &&
+            (boxtext[e[0]].innerText !== "")) {
+            
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " won"
+            isgameover = true;
+            gameover.play();  // Play game over sound
+            document.querySelector('.imgbox').getElementsByTagName("img")[0].style.width = "200px";
+            
+            // Auto reset the game after 3 seconds
+            setTimeout(() => {
+                resetGame();
+            }, 3000);
+        }
+    })
 }
 
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
+Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '' && !isgameover) {
             boxtext.innerText = turn;
             turn = changeTurn();
             audioTurn.play();
             checkwin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
         }
     })
 })
 
-reset.addEventListener('click', ()=>{
+// Function to reset the game
+const resetGame = () => {
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
         element.innerText = ""
     });
     turn = "X";
-    isgameover = false
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName("img")[0].style.width = "0px"
-    
-})
+    isgameover = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName("img")[0].style.width = "0px";
+}
+
+// Add onclick listener to reset button
+reset.addEventListener('click', resetGame);
